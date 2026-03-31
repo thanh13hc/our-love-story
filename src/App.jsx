5610fc846bb075a592e9930f76cd17b5556101c9
tom <thanh13hc@gmail.com>
2026-04-01 00:33:53 +0700

    feat: add lenis smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import {
-  GsapFrom,
-  GsapFromTo,
-  GsapScrollTrigger,
-  GsapStagger,
-  GsapText,
-  GsapTimeline,
-  GsapTo,
-  Home,
-} from "./pages";
-import PlayGround from "./pages/PlayGround";
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+import { Home } from "./pages";
 
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
@@ -26,6 +23,29 @@ if (typeof window !== "undefined" && history.scrollRestoration) {
 }
 
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      smoothWheel: true,
+      duration: 1.2,
+    });
+
+    const onScroll = () => ScrollTrigger.update();
+    lenis.on("scroll", onScroll);
+
+    let rafId;
+    const raf = (time) => {
+      lenis.raf(time);
+      rafId = requestAnimationFrame(raf);
+    };
+    rafId = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(rafId);
+      lenis.off("scroll", onScroll);
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <div className="min-h-screen h-full w-full flex justify-center">
       <Router>
@@ -38,6 +58,7 @@ const App = () => {
           <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
           <Route path="/gsaptext" element={<GsapText />} /> */}
           <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate path="/" replace />} />
           {/* <Route path="/playground" element={<PlayGround />} /> */}
         </Routes>
       </Router>
